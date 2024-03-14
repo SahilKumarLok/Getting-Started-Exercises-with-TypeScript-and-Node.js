8f75cd5b0a8abdf5e217663c96be8ad7e8d3cb80
SahilKumarLok <sk8579112@gmail.com>
2024-03-14 17:00:10 +0500

    q 10 completed

--- day 4/10/10.js
+++ day 4/10/10.js
@@ -1,7 +1,13 @@
 "use strict";
 // Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
-// using question 4 and 5 
+// using question 4 and 6
 //Q4
+// Sahil Kumar, 2024-03-10
+// this program prints a quote on passion by Oprah Winfrey  
 console.log(`Oprah Winfrey once said, "Passion is energy. Feel the power that comes from focusing on what excites you."`);
-//Q5
-console.log(`Oprah Winfrey once said, "Passion is energy. Feel the power that comes from focusing on what excites you."`);
+//06
+// Sahil Kumar, 2024-03-10
+// this program show how to make space or put the object in next line and how to overcome it 
+let p = "\t\n Sahil Kumar \t\n";
+console.log(p);
+console.log(p.trim());
