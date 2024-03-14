b3b18602485d21f48cb936fe255750fe324b5c58
SahilKumarLok <sk8579112@gmail.com>
2024-03-14 16:27:46 +0500

    when doing 10, found mistakes in 05

---
+++ day 4/10/10.ts
@@ -0,0 +1,11 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// using question 4 and 5 
+
+//Q4
+
+console.log(`Oprah Winfrey once said, "Passion is energy. Feel the power that comes from focusing on what excites you."`)
+
+//Q5
+
+console.log(`Oprah Winfrey once said, "Passion is energy. Feel the power that comes from focusing on what excites you."`)
