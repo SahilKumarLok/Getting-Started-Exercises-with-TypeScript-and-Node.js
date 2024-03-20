62c01eb073f8ce316c5fcab43468a9ad45951f1d
Sahil Kumar <sk8579112@gmail.com>
2024-03-20 16:48:40 +0500

    completed 11

---
+++ day 4/11/11.ts
@@ -0,0 +1,8 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let x = ["James", "John", "Jack"];
+
+for (let i = 0; i < x.length; i++) {
+    console.log(x[i]);
+}
+
