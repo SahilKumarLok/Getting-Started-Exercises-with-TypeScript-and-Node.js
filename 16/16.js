c193c57ab1e9ec47d3937fb92c99d97ac8f6a269
Sahil Kumar <sk8579112@gmail.com>
2024-03-25 13:05:46 +0500

    16 completed

--- 16/16.js
+++ 16/16.js
@@ -1,1 +1,11 @@
 "use strict";
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+let guests = ["Joseph", "Marie", "Nike"];
+console.log("Great news! I found a bigger dinner table!");
+// Adding more guests
+guests.unshift("jake");
+guests.splice(guests.length / 2, 0, "Emile");
+guests.push("Johnson");
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+});
