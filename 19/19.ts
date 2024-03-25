07d4b748f62949281767a8dd9942fc860bc25b60
Sahil Kumar <sk8579112@gmail.com>
2024-03-25 20:18:37 +0500

    Q19 completed

---
+++ 19/19.ts
@@ -0,0 +1,4 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let guestes: string[] = [ 'John', "Jack", "Johnson", "Joseph", "Jerry"];
+console.log(`I am inviting ${guestes.length} people to dinner.`);
