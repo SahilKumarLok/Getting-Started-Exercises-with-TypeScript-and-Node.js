b153b55765df46a6522e84670f85ac6f59cd16be
Sahil Kumar <sk8579112@gmail.com>
2024-03-25 20:09:43 +0500

    completed 18

---
+++ 18/18.ts
@@ -0,0 +1,25 @@
+// Seeing the World: Think of at least five places in the world you’d like to visit
+
+let places: string[] = ["China", "Canada", "Italy", "Germany", "Austria" ];
+
+console.log("Original order:", places);
+
+console.log("Alphabetical order:", [...places].sort());
+
+console.log("Original order:", places);
+
+console.log("Reverse alphabetical order:", [...places].sort().reverse());
+
+console.log("Original order:", places);
+
+places.reverse();
+console.log("Reversed order:", places);
+
+places.reverse();
+console.log("Original order:", places);
+
+places.sort();
+console.log("Alphabetical order:", places);
+
+places.reverse();
+console.log("Reverse alphabetical order:", places);
