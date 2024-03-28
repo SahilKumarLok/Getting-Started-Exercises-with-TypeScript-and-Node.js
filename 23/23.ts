20c1409c26920a2cf55c7ad92c4e0ba4d2a9356b
Sahil Kumar <sk8579112@gmail.com>
2024-03-28 10:35:57 +0500

    23 completed

---
+++ 23/23.ts
@@ -0,0 +1,33 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+
+let favoriteLanguage = "JavaScript";
+
+console.log("Is favoriteLanguage == 'JavaScript'? I predict True.");
+console.log(favoriteLanguage == "JavaScript"); // True
+
+console.log("Is favoriteLanguage == 'javascript'? I predict False.");
+console.log(favoriteLanguage == "javascript"); // False
+
+console.log("Is favoriteLanguage === 'JavaScript'? I predict True.");
+console.log(favoriteLanguage === "JavaScript"); // True
+
+console.log("Is favoriteLanguage === 'javascript'? I predict False.");
+console.log(favoriteLanguage === "javascript"); // False
+
+console.log("Is favoriteLanguage != 'Java'? I predict True.");
+console.log(favoriteLanguage != "Java"); // True
+
+console.log("Is favoriteLanguage !== 'JavaScript'? I predict False.");
+console.log(favoriteLanguage !== "JavaScript"); // False
+
+console.log("Is favoriteLanguage > 'TypeScript'? I predict False.");
+console.log(favoriteLanguage > "TypeScript"); // False
+
+console.log("Is favoriteLanguage < 'Python'? I predict True.");
+console.log(favoriteLanguage < "Python"); // True
+
+console.log("Is favoriteLanguage >= 'JavaScript'? I predict True.");
+console.log(favoriteLanguage >= "JavaScript"); // True
+
+console.log("Is favoriteLanguage <= 'Python'? I predict True.");
+console.log(favoriteLanguage <= "Python"); // True
