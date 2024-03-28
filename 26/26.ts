3575aa4ddcfc1d8c34822eaffb37bce105c83b82
Sahil Kumar <sk8579112@gmail.com>
2024-03-28 23:28:20 +0500

    26 completed

---
+++ 26/26.ts
@@ -0,0 +1,19 @@
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+
+// Version 1: runs the if block
+let alienColor: string = 'green';
+
+if (alienColor === 'green') {
+    console.log("You just earned 5 points for shooting the green alien!");
+} else {
+    console.log("You just earned 10 points for shooting the red alien!");
+}
+
+// Version 2: runs the else block
+alienColor = 'red';
+
+if (alienColor === 'green') {
+    console.log("You just earned 5 points for shooting the green alien!");
+} else {
+    console.log("You just earned 10 points for shooting the red alien!");
+}
