0bf3dbd6d6aa4fc19207243ea2f4bc6195b0b079
Sahil Kumar <sk8579112@gmail.com>
2024-03-28 23:50:43 +0500

    27 completed and also found new method which need to be learned

---
+++ 27/27.ts
@@ -0,0 +1,50 @@
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+
+let alienColor_green: string = 'green';
+
+if (alienColor_green === 'green') {
+    console.log('You just earned 5 points!');
+} else if (alienColor_green === 'yellow') {
+    console.log('You just earned 10 points!');
+} else if (alienColor_green === 'red') {
+    console.log('You just earned 15 points!');
+}
+
+let alienColor_yellow: string = 'yellow';
+
+if (alienColor_yellow === 'green') {
+    console.log('You just earned 5 points!');
+} else if (alienColor_yellow === 'yellow') {
+    console.log('You just earned 10 points!');
+} else if (alienColor_yellow === 'red') {
+    console.log('You just earned 15 points!');
+}
+
+let alienColor_red: string = 'red';
+
+if (alienColor_red === 'green') {
+    console.log('You just earned 5 points!');
+} else if (alienColor_red === 'yellow') {
+    console.log('You just earned 10 points!');
+} else if (alienColor_red === 'red') {
+    console.log('You just earned 15 points!');
+}
+
+// advance version, need to be learned.
+// Define the alien color
+// let alienColors: string = 'green';
+
+// // Write a switch statement to check the alien color
+// switch (alienColors) {
+//     case 'green':
+//         console.log('You just earned 5 points!');
+//         break;
+//     case 'yellow':
+//         console.log('You just earned 10 points!');
+//         break;
+//     case 'red':
+//         console.log('You just earned 15 points!');
+//         break;
+//     default:
+//         console.log('Invalid alien color.');
+// }
