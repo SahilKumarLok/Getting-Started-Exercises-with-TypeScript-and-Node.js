dc6f066da2748c9dc59836f92cfee9c5f6be7369
Sahil Kumar <sk8579112@gmail.com>
2024-03-28 23:22:30 +0500

    25 completed

---
+++ 25/25.js
@@ -0,0 +1,12 @@
+"use strict";
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// Variant that passes the if test
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points!");
+}
+// Variant that fails the if test
+alien_color = 'red';
+if (alien_color === 'green') {
+    console.log("You just earned 5 points!");
+}
