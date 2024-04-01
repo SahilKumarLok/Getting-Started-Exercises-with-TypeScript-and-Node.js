1dab3840f075b11a40e13d460f336e35847d3803
Sahil Kumar <sk8579112@gmail.com>
2024-04-01 22:46:41 +0500

    29 completed

---
+++ 29/29.ts
@@ -0,0 +1,16 @@
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+
+let favorite_fruits: string[] = ["apples", "bananas", "cherries", "Oranges"];
+
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like Cherries!");
+}
+if (favorite_fruits.includes("Oranges")) {
+    console.log("You really like Oranges!");
+}
