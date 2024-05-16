d696b11745abac1374662476f36a6ac5d807a720
SahilKumarLok <sk8579112@gmail.com>
2024-05-16 21:11:54 +0500

    32 completed

--- 32/32.js
+++ 32/32.js
@@ -3,7 +3,8 @@
 let current_users = ["user1", "admin", "user3", "user4", "user5"];
 let new_users = ["User1", "User6", "user7", "admin", "User9"];
 new_users.forEach(newUser => {
-    if (current_users.some(currentUser => currentUser.toLowerCase() === newUser.toLowerCase())) {
+    let our_condition = current_users.some(currentUser => currentUser.toLowerCase() === newUser.toLowerCase());
+    if (our_condition) {
         console.log(`${newUser} will need to enter a new username.`);
     }
     else {
