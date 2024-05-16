f91bd5365b91e47485ff9c4de9c620acba8c81c3
SahilKumarLok <sk8579112@gmail.com>
2024-05-16 20:53:29 +0500

    31 completed

--- 31/31.ts
+++ 31/31.ts
@@ -1,9 +1,15 @@
+// No Users
 
-
-let username: string[] = [];
-
-if (username.length === 0) {
-    console.log("We need to find some users!");
-} else {
-    console.log ("We Have a lot of users!");
+let userNames = ["Admin", "user01", "user02", "user03", "user04"];
+userNames = [];
+if (userNames.length === 0){
+    console.log("Your Array is empty, We need to find some users! ")
+}else{
+    userNames.forEach(userNames => {
+        if (userNames === "admin") {
+            console.log("Hello admin, would you like to see a status report?");
+        } else {
+            console.log(`Hello ${userNames}, thank you for logging in again.`);
+        }
+    });
 }
