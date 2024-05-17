34a3e3a2419f6da7b6dffa12a3f476e3ea10c23c
SahilKumarLok <sk8579112@gmail.com>
2024-05-17 13:29:06 +0500

    34 completed

---
+++ 34/34.js
@@ -0,0 +1,8 @@
+"use strict";
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+let pizza = ["Chicken Tika", "Malai Cheese", "Fajita"];
+for (let onePizza of pizza) {
+    console.log(`I like ${onePizza} pizza`);
+}
+;
+console.log("I live pizzas");
