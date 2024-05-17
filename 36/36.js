80b7457dc6fb5fde8cd5d37f4c50a1cca31f6aa8
SahilKumarLok <sk8579112@gmail.com>
2024-05-17 20:39:45 +0500

    36 completed

---
+++ 36/36.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_shirt(size, printMessage) {
+    console.log(`Your Selected ${size} size shirt with ${printMessage} print on shirt`);
+}
+make_shirt("Medium", "Hello World!");
