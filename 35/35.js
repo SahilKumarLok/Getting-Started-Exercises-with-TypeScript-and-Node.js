7f78ec5cbe2e5371f243e4ee1e3cc0f666e1d48f
SahilKumarLok <sk8579112@gmail.com>
2024-05-17 13:38:27 +0500

    35 completed

---
+++ 35/35.js
@@ -0,0 +1,7 @@
+"use strict";
+let petAnimals = ["Cat", "Dog", "Parrot"];
+for (let onePet of petAnimals) {
+    console.log(`A ${onePet} would make a great pet.`);
+}
+;
+console.log("Any of these animals would make a great pet!");
