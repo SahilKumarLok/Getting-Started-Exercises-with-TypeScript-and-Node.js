48898df61e4e0349f7e6fb42a4359c763a7ccd59
SahilKumarLok <sk8579112@gmail.com>
2024-05-18 18:37:02 +0500

    37 completed

---
+++ 37/37.js
@@ -0,0 +1,8 @@
+"use strict";
+//Large Shirts
+function make_shirts(size = "Large", printMessage = "I Love TypScript") {
+    console.log(`Creating a ${size} shirt with the ${printMessage}`);
+}
+;
+make_shirts();
+make_shirts("medium");
---
+++ 37/37.ts
@@ -0,0 +1,7 @@
+//Large Shirts
+function make_shirts (size:string = "Large", printMessage: string = "I Love TypScript"){
+    console.log(`Creating a ${size} shirt with the ${printMessage}`)
+};
+make_shirts();
+
+make_shirts("medium");
