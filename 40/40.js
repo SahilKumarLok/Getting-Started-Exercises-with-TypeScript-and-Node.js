07107b52a36fa557de982d396d350926a4dd5ae1
SahilKumarLok <sk8579112@gmail.com>
2024-05-19 15:31:01 +0500

    40 completed

---
+++ 40/40.js
@@ -0,0 +1,19 @@
+"use strict";
+function make_album(artist_name, album_title, tracks) {
+    let album = {
+        artist: artist_name,
+        title: album_title,
+    };
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    ;
+    return album;
+}
+;
+let album1 = make_album("Artist1", "Album Title 1");
+let album2 = make_album("Artist2", "Album Title 2");
+let album3 = make_album("Artist3", "Album Title 3", 10);
+console.log(album1);
+console.log(album2);
+console.log(album3);
